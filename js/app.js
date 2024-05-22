66cd4456dd7346e335ce6a3c523f1d15c4c12d75
Jose Costa <carlos_jesus_costa@hotmail.com>
2024-05-22 21:20:57 +0100

    SetDaily limit/ Reset Day

--- js/app.js
+++ js/app.js
@@ -1,87 +1,6 @@
-class CalorieTracker {
-  constructor() {
-    this._calorieLimit = 2000;
-    this._totalCalories = 0;
-    this._meals = [];
-    this._workouts = [];
-
-    this._displayCaloriesTotal();
-    this._displayCaloriesLimit();
-    this._displayCaloriesConsumed();
-    this._displayCaloriesBurned();
-    this._displayCaloriesRemaining();
-  }
-  addMeal(meal) {
-    this._meals.push(meal);
-    this._totalCalories += meal.calories;
-    this._render();
-  }
-
-  addWorkout(workout) {
-    this._workouts.push(workout);
-    this._totalCalories -= workout.calories;
-    this._render();
-  }
-
-  _displayCaloriesTotal() {
-    const totalCaloriesEl = document.getElementById("calories-total");
-    totalCaloriesEl.innerHTML = this._totalCalories;
-  }
-
-  _displayCaloriesLimit() {
-    const limitCaloriesEl = document.getElementById("calories-limit");
-    limitCaloriesEl.innerHTML = this._calorieLimit;
-  }
-
-  _displayCaloriesConsumed() {
-    const caloriesConsumedEl = document.getElementById("calories-consumed");
-    const consumed = this._meals.reduce(
-      (total, meal) => total + meal.calories,
-      0
-    );
-    caloriesConsumedEl.innerHTML = consumed;
-  }
-
-  _displayCaloriesBurned() {
-    const caloriesBurnedEl = document.getElementById("calories-burned");
-    const burned = this._workouts.reduce(
-      (total, workout) => total + workout.calories,
-      0
-    );
-
-    caloriesBurnedEl.innerHTML = burned;
-  }
-
-  _displayCaloriesRemaining() {
-    const caloriesRemainingEl = document.getElementById("calories-remaining");
-    const remaining = this._calorieLimit - this._totalCalories;
-
-    caloriesRemainingEl.innerHTML = remaining;
-  }
-
-  _render() {
-    this._displayCaloriesTotal();
-    this._displayCaloriesConsumed();
-    this._displayCaloriesBurned();
-    this._displayCaloriesRemaining();
-  }
-}
-
-class Meal {
-  constructor(name, calories) {
-    this.id = Math.random().toString(16).slice(2);
-    this.name = name;
-    this.calories = calories;
-  }
-}
-
-class Workout {
-  constructor(name, calories) {
-    this.id = Math.random().toString(16).slice(2);
-    this.name = name;
-    this.calories = calories;
-  }
-}
+const dailyLimit = document.getElementById("limit-form");
+const input = document.getElementById("limit");
+const resetDay = document.getElementById("reset");
 
 const tracker = new CalorieTracker();
 
@@ -93,5 +12,12 @@ tracker.addMeal(lunch);
 const run = new Workout("Morning run", 330);
 tracker.addWorkout(run);
 
-console.log(tracker._meals);
-console.log(tracker._workouts);
+function setDailyValues(e) {
+  e.preventDefault();
+
+  const resetDaily = new ResetValues(input.value, 0);
+  tracker.resetDailyLimit(resetDaily);
+}
+
+dailyLimit.addEventListener("submit", setDailyValues);
+resetDay.addEventListener("click", setDailyValues);
