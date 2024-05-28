ab8cab0bc76de2c4430a472bed1e1f04ab6e3416
Jose Costa <carlos_jesus_costa@hotmail.com>
2024-05-28 20:14:14 +0100

    feature progress bar

--- js/app.js
+++ js/app.js
@@ -3,14 +3,17 @@ class App {
     this.dailyLimit = document.getElementById("limit-form");
     this.addMealForm = document.getElementById("meal-form");
     this.resetDay = document.getElementById("reset");
+    this.addWorkoutForm = document.getElementById("workout-form");
 
     this.tracker = new CalorieTracker();
     this.addMeal = this.addMeal.bind(this);
     this.setDailyValues = this.setDailyValues.bind(this);
+    this.addWorkout = this.addWorkout.bind(this);
 
     this.dailyLimit.addEventListener("submit", this.setDailyValues);
     this.resetDay.addEventListener("click", this.setDailyValues);
     this.addMealForm.addEventListener("submit", this.addMeal);
+    this.addWorkoutForm.addEventListener("submit", this.addWorkout);
   }
 
   setDailyValues(e) {
@@ -41,6 +44,24 @@ class App {
     );
     this.tracker.addMeal(addMeal);
     meal.value = "";
+    calory.value = "";
+  }
+
+  addWorkout(e) {
+    e.preventDefault();
+    const workout = document.getElementById("workout-name");
+    const calory = document.getElementById("workout-calories");
+
+    if (!workout.value || !calory.value) {
+      return alert("Need to Fill all the sections");
+    }
+
+    const addWorkout = new Workout(
+      workout.value[0].toUpperCase() + workout.value.slice(1),
+      parseInt(calory.value)
+    );
+    this.tracker.addWorkout(addWorkout);
+    workout.value = "";
     calory.value = "";
   }
 }
